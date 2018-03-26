f2cce2793814b93e1ac8fd590310f7295ae2b8a9
randomness38 <randomness.jay@gmail.com>
2018-03-26 19:50:41 +0900

    add formReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,40 +1,8 @@
-import * as actions from '../actions'
+import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+import decks from './decks'
 
-
-export default function decks (state = {}, action) {
-    switch (action.type) {
-        case actions.RECEIVE_DECKS :
-            return {
-                ...state,
-                ...action.decks,
-            }
-        case actions.CLEAR_DECKS :
-            return {
-                state : undefined
-            }
-        case actions.LOAD_DECK :
-            return {
-                ...state,
-                ...action.deck
-            }
-        case actions.REMOVE_DECK :
-            return {
-                ...state,
-                [action.deckId] : undefined
-            }
-        case actions.LOAD_CARD :
-            return {
-                ...state,
-                [action.deckId] : {
-                    ...state[action.deckId] ,
-                    questions : [
-                        ...state[action.deckId].questions,
-                        action.card
-                    ]
-                }
-            }
-        default :
-            return state
-    }
-}
-
+export default combineReducers({
+    decks,
+    form: formReducer
+});
